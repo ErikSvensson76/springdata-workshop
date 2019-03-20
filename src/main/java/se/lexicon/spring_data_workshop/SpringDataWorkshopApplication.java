b70019716302f7a35d2e4ac6de9f16e2d87e019c
Erik Svensson <eosvensson@gmail.com>
2019-03-20 10:45:10 +0100

    First commit

---
+++ src/main/java/se/lexicon/spring_data_workshop/SpringDataWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.spring_data_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/spring_data_workshop/SpringDataWorkshopApplicationTests.java
@@ -0,0 +1,16 @@
+package se.lexicon.spring_data_workshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataWorkshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
